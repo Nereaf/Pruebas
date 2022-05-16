8f67ae7bdbafa821da4348bc146ba8b001b95072
ToMaIg <100483663+ToMaIg@users.noreply.github.com>
2022-05-16 22:14:36 +0200

    Update GestorAlarmasTest.java

--- test/pruebas/GestorAlarmasTest.java
+++ test/pruebas/GestorAlarmasTest.java
@@ -27,6 +27,7 @@ class GestorAlarmasTest {
     static void setUpBeforeClass() throws Exception{
         GAS = new GestorAlarmas();
     }
+	@DisplayName("CrudAlarma [PRB-007]")
 	@Nested
 	class crudAlarma{
 		@BeforeEach
@@ -37,41 +38,41 @@ class GestorAlarmasTest {
 
 		@AfterEach
 		void tearDown() throws Exception {
-			bbdd.getAlarmas().clear(); //Como está implementado o patrón Singleton, é necesario eliminar as alarmas porque a BBDD vai seguir sendo a mesma
+			bbdd.getAlarmas().clear(); //Como estÃ¡ implementado o patrÃ³n Singleton, Ã© necesario eliminar as alarmas porque a BBDD vai seguir sendo a mesma
 			bbdd=null;
 		}
 		
 		@Test
-		@DisplayName("CPB-002-01")
+		@DisplayName("CPB-007-01")
 		void testCrudAlarma01() {
 			alarmaP=GAS.crudAlarma(alarmaI, 0);
 			assertSame(alarmaI, alarmaP, "Las alarmas non son la misma");
 		}
 		
 		@Test
-		@DisplayName("CPB-002-02")
+		@DisplayName("CPB-007-02")
 		void testCrudAlarma02() {
 			alarmaP=GAS.crudAlarma(alarmaI, 1);
 			assertSame(alarmaI, alarmaP, "Las alarmas non son la misma");
-			assertTrue(bbdd.getAlarmas().contains(alarmaI), "O arraylist non contén a alarma");
+			assertTrue(bbdd.getAlarmas().contains(alarmaI), "O arraylist non contÃ©n a alarma");
 		}
 		
 		@Test
-		@DisplayName("CPB-002-03")
+		@DisplayName("CPB-007-03")
 		void testCrudAlarma03() {
 			alarmaP=GAS.crudAlarma(alarmaI, 2);
 			assertSame(alarmaI, alarmaP, "Las alarmas non son la misma");
 		}
 		
 		@Test
-		@DisplayName("CPB-002-04")
+		@DisplayName("CPB-007-04")
 		void testCrudAlarma04() {
 			alarmaP=GAS.crudAlarma(alarmaI, 3);
 			assertSame(alarmaI, alarmaP, "Las alarmas non son la misma");
 		}
 		
 		@Test
-		@DisplayName("CPB-002-05")
+		@DisplayName("CPB-007-05")
 		void testCrudAlarma05() {
 			alarmaR=new Alarma(0, "Alarma de Robos");
 			bbdd.getAlarmas().add(alarmaI); //Insertamos un elemento
@@ -80,7 +81,7 @@ class GestorAlarmasTest {
 		}
 		
 		@Test
-		@DisplayName("CPB-002-06")
+		@DisplayName("CPB-007-06")
 		void testCrudAlarma06() {
 			alarmaR=new Alarma(0, "Alarma de Robos");
 			bbdd.getAlarmas().add(alarmaI); //Insertamos un elemento
@@ -91,7 +92,7 @@ class GestorAlarmasTest {
 		}
 		
 		@Test
-		@DisplayName("CPB-002-07")
+		@DisplayName("CPB-007-07")
 		void testCrudAlarma07() {
 			alarmaR=new Alarma(0, "Alarma de Robos");
 			bbdd.getAlarmas().add(alarmaI); //Insertamos un elemento
@@ -102,7 +103,7 @@ class GestorAlarmasTest {
 		}
 		
 		@Test
-		@DisplayName("CPB-002-08")
+		@DisplayName("CPB-007-08")
 		void testCrudAlarma08() {
 			alarmaR=new Alarma(1, "Alarma de Robos");
 			bbdd.getAlarmas().add(alarmaI); //Insertamos un elemento
@@ -111,7 +112,7 @@ class GestorAlarmasTest {
 		}
 		
 		@Test
-		@DisplayName("CPB-002-09")
+		@DisplayName("CPB-007-09")
 		void testCrudAlarma09() {
 			alarmaR=new Alarma(1, "Alarma de Robos");
 			bbdd.getAlarmas().add(alarmaI); //Insertamos un elemento
@@ -121,7 +122,7 @@ class GestorAlarmasTest {
 		}
 		
 		@Test
-		@DisplayName("CPB-002-10")
+		@DisplayName("CPB-007-10")
 		void testCrudAlarma10() {
 			alarmaR=new Alarma(1, "Alarma de Robos");
 			bbdd.getAlarmas().add(alarmaI); //Insertamos un elemento
@@ -131,19 +132,20 @@ class GestorAlarmasTest {
 		}
 	}
 	@Nested
-	@DisplayName("Recibir notificación alerta [PRB-004]")
+	@DisplayName("Recibir notificaciÃ³n alerta [PRB-004]")
 	class recibirNotificacion{
 		@BeforeEach
 		void setUp() throws Exception {
 			alarm = new Alarma(1, "Tormenta");
 			alert=new Alerta("Activa", 1, posAlert, "ETSE", new Date(), alarm);
 		}
+		@DisplayName("Prueba [CPB-004-01]")
 		@Test
 		void testCrearAlerta() {
 			Alerta alerta2 = alert.crearAlerta("Activa", "ETSE", new Date(),alarm);
 			assertEquals(alert,alerta2,"La alerta creada es diferente");
 		}
-		
+		@DisplayName("Prueba [CPB-003-02]")
 		@Test
 		void testRecibirAlerta() {
 		Alerta alerta2=alert.crearAlerta("Activa", "ETSE", new Date(),alarm);
@@ -175,9 +177,10 @@ class GestorAlarmasTest {
 
 		@AfterEach
 		void tearDown() throws Exception {
-			bbdd.getAlarmas().clear(); //Como está implementado o patrón Singleton, é necesario eliminar as alarmas porque a BBDD vai seguir sendo a mesma
+			bbdd.getAlarmas().clear(); //Como estÃ¡ implementado o patrÃ³n Singleton, Ã© necesario eliminar as alarmas porque a BBDD vai seguir sendo a mesma
 			bbdd=null;
 		}
+		@DisplayName("Prueba [CPB-008-01]")
 		@Test
 		void testCrudProtocolo1() {
 			
@@ -185,7 +188,7 @@ class GestorAlarmasTest {
 			Protocolo protocolo1in = GAS.crudProtocolo(protocolo1, 0);
 			assertEquals(protocolo1,protocolo1in,"Los protocolos no son iguales");
 		}
-		
+		@DisplayName("Prueba [CPB-008-02]")
 		@Test
 		void testCrudProtocolo2() {
 			
@@ -193,7 +196,7 @@ class GestorAlarmasTest {
 			Protocolo protocolo1in = GAS.crudProtocolo(protocolo1, 1);
 			assertEquals(protocolo1,protocolo1in,"Los protocolos no son iguales");
 		}
-		
+		@DisplayName("Prueba [CPB-008-03]")
 		@Test
 		void testCrudProtocolo3() {
 
@@ -201,20 +204,20 @@ class GestorAlarmasTest {
 			Protocolo protocolo2 = new Protocolo(1,"Encender bomba de achique",3);
 			Protocolo protocolo1in = GAS.crudProtocolo(protocolo1, 1);
 			Protocolo protocolo2in = GAS.crudProtocolo(protocolo2, 2);
-			assertEquals(protocolo1in.getTipo(),protocolo2in.getTipo(),"No se modificó correctamente el tipo del protocolo");
-			assertEquals(protocolo1in.getDescripcion(),protocolo2in.getDescripcion(),"No se modificó correctamente la descripción del protocolo");
+			assertEquals(protocolo1in.getTipo(),protocolo2in.getTipo(),"No se modificÃ³ correctamente el tipo del protocolo");
+			assertEquals(protocolo1in.getDescripcion(),protocolo2in.getDescripcion(),"No se modificÃ³ correctamente la descripciÃ³n del protocolo");
 		}
-		
+		@DisplayName("Prueba [CPB-008-04]")
 		@Test
 		void testCrudProtocolo4() {
 			
 			Protocolo protocolo1 = new Protocolo(1,"Utilizar extintor para apagar incendio",1);
 			Protocolo protocolo1in = GAS.crudProtocolo(protocolo1, 1);
 			Protocolo protocolo2in = GAS.crudProtocolo(protocolo1, 3);
-			assertEquals(protocolo1in,protocolo2in,"No se borró el protocolo indicado");
+			assertEquals(protocolo1in,protocolo2in,"No se borrÃ³ el protocolo indicado");
 			
 		}
-		
+		@DisplayName("Prueba [CPB-008-05]")
 		@Test
 		void testCrudProtocolo5() {
 		
@@ -222,11 +225,11 @@ class GestorAlarmasTest {
 			Protocolo protocolo2 = new Protocolo(1,"Utilizar manguera",1);
 			bbdd.getProtocolos().add(protocolo1);
 			Protocolo protocoloaux = GAS.crudProtocolo(protocolo2, 0);
-			assertTrue(bbdd.getProtocolos().contains(protocolo1),"El protocolo no está en la BBDD");
-			assertFalse(bbdd.getProtocolos().contains(protocolo2),"El protocolo está en la BBDD");
+			assertTrue(bbdd.getProtocolos().contains(protocolo1),"El protocolo no estÃ¡ en la BBDD");
+			assertFalse(bbdd.getProtocolos().contains(protocolo2),"El protocolo estÃ¡ en la BBDD");
 			
 		}
-		
+		@DisplayName("Prueba [CPB-008-06]")
 		@Test
 		void testCrudProtocolo6() {
 		
@@ -235,11 +238,11 @@ class GestorAlarmasTest {
 			bbdd.getProtocolos().add(protocolo1);
 			Protocolo protocoloaux=GAS.crudProtocolo(protocolo2, 2);
 			assertEquals(protocolo2,protocoloaux,"Los protocolos no son iguales");
-			assertTrue(bbdd.getProtocolos().contains(protocolo1),"El protocolo no está en la BBDD");
-			assertFalse(bbdd.getProtocolos().contains(protocolo2),"El protocolo está en la BBDD");
+			assertTrue(bbdd.getProtocolos().contains(protocolo1),"El protocolo no estÃ¡ en la BBDD");
+			assertFalse(bbdd.getProtocolos().contains(protocolo2),"El protocolo estÃ¡ en la BBDD");
 			assertEquals(protocolo1.getDescripcion(),protocolo2.getDescripcion(),"La descripcion no se ha modificado");
 		}
-		
+		@DisplayName("Prueba [CPB-008-07]")
 		@Test
 		void testCrudProtocolo7() {
 		
@@ -251,7 +254,7 @@ class GestorAlarmasTest {
 			assertFalse(bbdd.getProtocolos().contains(protocolo1));
 		
 		}
-		
+		@DisplayName("Prueba [CPB-008-08]")
 		@Test
 		void testCrudProtocolo8() {
 		
@@ -264,7 +267,7 @@ class GestorAlarmasTest {
 			assertTrue(bbdd.getProtocolos().contains(protocolo1));
 			assertFalse(bbdd.getProtocolos().contains(protocolo2));
 		}
-		
+		@DisplayName("Prueba [CPB-008-09]")
 		@Test
 		void testCrudProtocolo9() {
 		
@@ -273,9 +276,9 @@ class GestorAlarmasTest {
 			bbdd.getProtocolos().add(protocolo1);
 			Protocolo protocoloaux=GAS.crudProtocolo(protocolo2, 2);
 			assertEquals(protocolo2, protocoloaux,"Los protocolos no son iguales");
-			assertTrue(bbdd.getProtocolos().contains(protocolo1),"El protocolo no está en la BBDD");
+			assertTrue(bbdd.getProtocolos().contains(protocolo1),"El protocolo no estÃ¡ en la BBDD");
 		}
-		
+		@DisplayName("Prueba [CPB-008-10]")
 		@Test
 		void testCrudProtocolo10() {
 		
@@ -284,7 +287,7 @@ class GestorAlarmasTest {
 			bbdd.getProtocolos().add(protocolo1);
 			Protocolo protocoloaux=GAS.crudProtocolo(protocolo2, 3);
 			assertEquals(protocolo2, protocoloaux,"Los protocolos no son iguales");
-			assertTrue(bbdd.getProtocolos().contains(protocolo1),"El protocolo no está en la BBDD");
+			assertTrue(bbdd.getProtocolos().contains(protocolo1),"El protocolo no estÃ¡ en la BBDD");
 			
 		}
 	}
@@ -300,37 +303,43 @@ class GestorAlarmasTest {
 
 		@AfterEach
 		void tearDown() throws Exception {
-			bbdd.getAlarmas().clear(); //Como está implementado o patrón Singleton, é necesario eliminar as alarmas porque a BBDD vai seguir sendo a mesma
+			bbdd.getAlarmas().clear(); //Como estÃ¡ implementado o patrÃ³n Singleton, Ã© necesario eliminar as alarmas porque a BBDD vai seguir sendo a mesma
 			bbdd=null;
 		}
+		@DisplayName("Prueba [CPB-009-01]")
 		@Test
 		void testInformarAlarma1() {
-			assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nº 1.");
+			assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nÂº 1.");
 		}
+		@DisplayName("Prueba [CPB-009-02]")
 		@Test
 		void testInformarAlarma2() {
 			MiembroEquipo me = new MiembroEquipo("Pablo","12345678X","p12","p12","pepe@terra.net",123456789,posUserV,"1","activo");
 			bbdd.getUsuarios().add(me);
-			assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nº 2.");
+			assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nÂº 2.");
 		}
+		@DisplayName("Prueba [CPB-009-03]")
 		@Test
 		void testInformarAlarma3() {
 			Usuario us = new Usuario("Ana","87654321Y","p11","p11","ana@terra.net",987654321);
 			bbdd.getUsuarios().add(us);
-			assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nº 3.");
+			assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nÂº 3.");
 		}
+		@DisplayName("Prueba [CPB-009-04]")
 		@Test
 		void testInformarAlarma4() {
 			MiembroUSC musc = new MiembroUSC("Ana","87654321Y","p11","p11","ana@terra.net",987654321,posUserF);
 			bbdd.getUsuarios().add(musc);
-			assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nº 4.");
+			assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nÂº 4.");
 		}
+		@DisplayName("Prueba [CPB-009-05]")
 		@Test
 		void testInformarAlarma5() {
 			MiembroUSC musc = new MiembroUSC("Ana","87654321Y","p11","p11","ana@terra.net",987654321,posUserV);
 			bbdd.getUsuarios().add(musc);
-			assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nº 5.");
+			assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nÂº 5.");
 		}
+		@DisplayName("Prueba [CPB-009-06]")
 		@Test
 		void testInformarAlarma6() {
 			MiembroUSC musc = new MiembroUSC("Ana","87654321Y","p31","p11","ana@terra.net",987654321,posUserV);
@@ -339,6 +348,7 @@ class GestorAlarmasTest {
 			bbdd.getUsuarios().add(musc2);
 			assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma recorriendo bucle 2 veces.");
 		}
+		@DisplayName("Prueba [CPB-009-07]")
 		@Test
 		void testInformarAlarma7() {
 			MiembroEquipo me = new MiembroEquipo("Pablo","12345678X","p12","p12","pepe@terra.net",123456789,posUserV,"1","activo");
@@ -350,42 +360,49 @@ class GestorAlarmasTest {
 		@Nested
 		@DisplayName("Pruebas de rendimiento [NFR-0002]")
 		class Rendimiento{
+			@DisplayName("Prueba [CPB-009-08]")
 			@Test 
 			void limiteTestInformarAlarmaRendimiento1() {
 				assertTimeoutPreemptively(Duration.ofSeconds(2), ()->{
 					testInformarAlarma1();
 				});
 			}
+			@DisplayName("Prueba [CPB-009-09]")
 			@Test
 			void limiteTestInformarAlarmaRendimiento2() {
 				assertTimeoutPreemptively(Duration.ofSeconds(2), ()->{
 					testInformarAlarma2();
 				});
 			}
+			@DisplayName("Prueba [CPB-009-10]")
 			@Test 
 			void limiteTestInformarAlarmaRendimiento3() {
 				assertTimeoutPreemptively(Duration.ofSeconds(2), ()->{
 					testInformarAlarma3();
 				});
 			}
+			@DisplayName("Prueba [CPB-009-11]")
 			@Test
 			void limiteTestInformarAlarmaRendimiento4() {
 				assertTimeoutPreemptively(Duration.ofSeconds(2), ()->{
 					testInformarAlarma4();
 				});
 			}
+			@DisplayName("Prueba [CPB-009-12]")
 			@Test 
 			void limiteTestInformarAlarmaRendimiento5() {
 				assertTimeoutPreemptively(Duration.ofSeconds(2), ()->{
 					testInformarAlarma5();
 				});
 			}
+			@DisplayName("Prueba [CPB-009-13]")
 			@Test 
 			void limiteTestInformarAlarmaRendimiento6() {
 				assertTimeoutPreemptively(Duration.ofSeconds(2), ()->{
 					testInformarAlarma6();
 				});
 			}
+			@DisplayName("Prueba [CPB-009-14]")
 			@Test 
 			void limiteTestInformarAlarmaRendimiento7() {
 				assertTimeoutPreemptively(Duration.ofSeconds(2), ()->{
@@ -395,3 +412,4 @@ class GestorAlarmasTest {
 		}
 	}
 }
+
