d3c5e6c3b2fb59f65b3fcdad6171a09998760afd
ToMaIg <100483663+ToMaIg@users.noreply.github.com>
2022-05-16 22:16:22 +0200

    Update TestSistemaEstadisticas.java

--- test/pruebas/TestSistemaEstadisticas.java
+++ test/pruebas/TestSistemaEstadisticas.java
@@ -5,35 +5,39 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestSistemaEstadisticas {
 
     SistemaEstadisticas sistema;
-
-    @BeforeEach
-    void setUpBeforeEach() throws Exception{
-        sistema = new SistemaEstadisticas();
-    }
-
-    @Test
-    @DisplayName("Prueba [CPB-005-01]")
-    void testRegistrarAlertas() {
-        Alerta alerta = new Alerta();
-        String mensaje = "Se han registrado 1 alertas.";
-        assertEquals(true, sistema.registrarAlerta(alerta));
-        assertEquals(mensaje, sistema.consultarEstadisticas());
-    }
-
-    @Test
-    @DisplayName("Prueba [CPB-005-02]")
-    void testAlertaNull() {
-        Alerta alertaNull = null;
-        String mensaje = "Se han registrado 0 alertas.";
-        assertEquals(true, sistema.registrarAlerta(alertaNull));
-        assertEquals(mensaje, sistema.consultarEstadisticas());
-    }
-
-    @Test
-    @DisplayName("Prueba [CPB-005-03]")
-    void testBBDDNoDisponible(){
-        sistema = null;
-        Alerta alerta = new Alerta();
-        assertEquals(false, sistema.registrarAlerta(alerta));
+    @DisplayName("RegistrarAlerta [PRB-005]")
+    @Nested
+    class registrarAlertas{
+	    @BeforeEach
+	    void setUpBeforeEach() throws Exception{
+	        sistema = new SistemaEstadisticas();
+	    }
+	
+	    @Test
+	    @DisplayName("Prueba [CPB-005-01]")
+	    void testRegistrarAlertas() {
+	        Alerta alerta = new Alerta();
+	        String mensaje = "Se han registrado 1 alertas.";
+	        assertEquals(true, sistema.registrarAlerta(alerta));
+	        assertEquals(mensaje, sistema.consultarEstadisticas());
+	    }
+	
+	    @Test
+	    @DisplayName("Prueba [CPB-005-02]")
+	    void testAlertaNull() {
+	        Alerta alertaNull = null;
+	        String mensaje = "Se han registrado 0 alertas.";
+	        assertEquals(true, sistema.registrarAlerta(alertaNull));
+	        assertEquals(mensaje, sistema.consultarEstadisticas());
+	    }
+	
+	    @Test
+	    @DisplayName("Prueba [CPB-005-03]")
+	    void testBBDDNoDisponible(){
+	        sistema = null;
+	        Alerta alerta = new Alerta();
+	        assertEquals(false, sistema.registrarAlerta(alerta));
+	    }
     }
 }
+
