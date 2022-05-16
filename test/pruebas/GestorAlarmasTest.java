2c89ceaa6422421720634211281ff6d1ff63cd67
ToMaIg <100483663+ToMaIg@users.noreply.github.com>
2022-05-16 15:51:40 +0200

    Update GestorAlarmasTest.java
    beforeall

--- test/pruebas/GestorAlarmasTest.java
+++ test/pruebas/GestorAlarmasTest.java
@@ -8,80 +8,58 @@ import java.util.Date;
 import org.junit.jupiter.api.*;
 
 class GestorAlarmasTest {
-
+	static GestorAlarmas GAS;
+	static Alarma alarm;
+	static Alerta alert;
+	static Float[] posAlert = {1.00f, 1.00f};
+	static Float[] posUserV = {1.00f, 1.00f};
+	static Float[] posUserF = {100.00f, 100.00f};
+	@BeforeAll
+    static void setUpBeforeClass() throws Exception{
+        GAS = new GestorAlarmas();
+        alarm = new Alarma(1, "Incendio");
+	alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
+    }
 	@Test
 	void testInformarAlarma1() {
-		Float[] posAlert = {1.00f, 1.00f};
-		Alarma alarm = new Alarma(1, "Incendio");
-		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
-		GestorAlarmas GAS = new GestorAlarmas();
 		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nº 1.");
 	}
 	@Test
 	void testInformarAlarma2() {
-		Float[] posAlert = {1.00f, 1.00f};
-		Float[] posUserV = {1.00f, 1.00f};
-		Alarma alarm = new Alarma(1, "Incendio");
-		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
 		MiembroEquipo me = new MiembroEquipo("Pablo","12345678X","p12","p12","pepe@terra.net",123456789,posUserV,"1","activo");
-		GestorAlarmas GAS = new GestorAlarmas();
 		BBDD.obtenerBBDD().getUsuarios().add(me);
 		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nº 2.");
 	}
 	@Test
 	void testInformarAlarma3() {
-		Float[] posAlert = {1.00f, 1.00f};
-		Alarma alarm = new Alarma(1, "Incendio");
-		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
 		Usuario us = new Usuario("Ana","87654321Y","p11","p11","ana@terra.net",987654321);
-		GestorAlarmas GAS = new GestorAlarmas();
 		BBDD.obtenerBBDD().getUsuarios().add(us);
 		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nº 3.");
 	}
 	@Test
 	void testInformarAlarma4() {
-		Float[] posAlert = {1.00f, 1.00f};
-		Float[] posUserF = {100.00f, 100.00f};
-		Alarma alarm = new Alarma(1, "Incendio");
-		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
 		MiembroUSC musc = new MiembroUSC("Ana","87654321Y","p11","p11","ana@terra.net",987654321,posUserF);
-		GestorAlarmas GAS = new GestorAlarmas();
 		BBDD.obtenerBBDD().getUsuarios().add(musc);
 		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nº 4.");
 	}
 	@Test
 	void testInformarAlarma5() {
-		Float[] posAlert = {1.00f, 1.00f};
-		Float[] posUserV = {1.00f, 1.00f};
-		Alarma alarm = new Alarma(1, "Incendio");
-		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
 		MiembroUSC musc = new MiembroUSC("Ana","87654321Y","p11","p11","ana@terra.net",987654321,posUserV);
-		GestorAlarmas GAS = new GestorAlarmas();
 		BBDD.obtenerBBDD().getUsuarios().add(musc);
 		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma camino nº 5.");
 	}
 	@Test
 	void testInformarAlarma6() {
-		Float[] posAlert = {1.00f, 1.00f};
-		Float[] posUserV = {1.00f, 1.00f};
-		Alarma alarm = new Alarma(1, "Incendio");
-		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
 		MiembroUSC musc = new MiembroUSC("Ana","87654321Y","p31","p11","ana@terra.net",987654321,posUserV);
 		MiembroUSC musc2 = new MiembroUSC("Pedro","1269183Y","p30","p10","pedro2@terra.net",981823321,posUserV);
-		GestorAlarmas GAS = new GestorAlarmas();
 		BBDD.obtenerBBDD().getUsuarios().add(musc);
 		BBDD.obtenerBBDD().getUsuarios().add(musc2);
 		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma recorriendo bucle 2 veces.");
 	}
 	@Test
 	void testInformarAlarma7() {
-		Float[] posAlert = {1.00f, 1.00f};
-		Float[] posUserV = {1.00f, 1.00f};
-		Alarma alarm = new Alarma(1, "Incendio");
-		Alerta alert=new Alerta("Urgente", 1, posAlert, "ETSE", new Date(), alarm);
 		MiembroEquipo me = new MiembroEquipo("Pablo","12345678X","p12","p12","pepe@terra.net",123456789,posUserV,"1","activo");
 		MiembroEquipo me2 = new MiembroEquipo("Pedro","8769183Y","p10","p10","pedro@terra.net",981823321,posUserV,"2","activo");
-		GestorAlarmas GAS = new GestorAlarmas();
 		BBDD.obtenerBBDD().getUsuarios().add(me);
 		BBDD.obtenerBBDD().getUsuarios().add(me2);
 		assertEquals(alert,GAS.informarAlarma(alert),"Error al informar alarma recorriendo bucle 2 veces.");
