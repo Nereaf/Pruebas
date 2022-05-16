d0d252066568243d3e0e39c65e08a80bcf00a863
ToMaIg <100483663+ToMaIg@users.noreply.github.com>
2022-05-16 15:50:22 +0200

    Update CentralitaTest.java
    beforeall

--- test/pruebas/CentralitaTest.java
+++ test/pruebas/CentralitaTest.java
@@ -8,11 +8,15 @@ import java.util.Date;
 import org.junit.jupiter.api.*;
 
 class CentralitaTest {
-
+	static GestorAlarmas GAS;
+	static Centralita cen;
+	@BeforeAll
+    static void setUpBeforeClass() throws Exception{
+        GAS = new GestorAlarmas();
+        cen = new Centralita(GAS);
+	}
 	@Test
 	void testNotificarAlerta1() {
-		GestorAlarmas GAS= new GestorAlarmas();
-		Centralita cen = new Centralita(GAS);
 		Float[] pos = {12.3f, 12.4f};
 		Alarma alarm = new Alarma(1, "Incendio");
 		Alerta alert=new Alerta("Urgente", 1, pos, "ETSE", new Date(), alarm);
@@ -20,8 +24,6 @@ class CentralitaTest {
 	}
 	@Test
 	void testNotificarAlerta2() {
-		GestorAlarmas GAS= new GestorAlarmas();
-		Centralita cen = new Centralita(GAS);
 		assertTrue(cen.notificarAlerta(null),"Error al notificar una alerta nula.");
 	}
 	@Nested
