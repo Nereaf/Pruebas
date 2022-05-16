7f540f661f96f0b70b4e67a61d0f3f9c46cd6eda
ToMaIg <100483663+ToMaIg@users.noreply.github.com>
2022-05-16 21:10:40 +0200

    Update Alarma.java
    equals

--- src/pruebas/Alarma.java
+++ src/pruebas/Alarma.java
@@ -36,5 +36,22 @@ public class Alarma {
 	public Integer getId() {
 		return this.id;
 	}
-	
+	@Override
+	public int hashCode() {
+		return Objects.hash(descripcion, id, protocolos);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alarma other = (Alarma) obj;
+		return Objects.equals(descripcion, other.descripcion) && Objects.equals(id, other.id)
+				&& Objects.equals(protocolos, other.protocolos);
+	}
 }
