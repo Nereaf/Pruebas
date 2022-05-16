fa144ee164a755bf9ada11bc81541fda931f1c0d
ToMaIg <100483663+ToMaIg@users.noreply.github.com>
2022-05-16 22:13:57 +0200

    Update CentralitaTest.java

--- test/pruebas/CentralitaTest.java
+++ test/pruebas/CentralitaTest.java
@@ -11,37 +11,45 @@ import org.junit.jupiter.api.*;
 class CentralitaTest {
 	static GestorAlarmas GAS;
 	static Centralita cen;
-	@BeforeAll
-    static void setUpBeforeClass() throws Exception{
-        GAS = mock(GestorAlarmas.class);
-        cen = new Centralita(GAS);
-	}
-	@Test
-	void testNotificarAlerta1() {
-		Float[] pos = {12.3f, 12.4f};
-		Alarma alarm = new Alarma(1, "Incendio");
-		Alerta alert=new Alerta("Urgente", 1, pos, "ETSE", new Date(), alarm);
-		assertTrue(cen.notificarAlerta(alert),"Error al notificar una alerta.");
-	}
-	@Test
-	void testNotificarAlerta2() {
-		assertTrue(cen.notificarAlerta(null),"Error al notificar una alerta nula.");
-	}
+	@DisplayName("Lanzar Alarmas [PRB-001]")
 	@Nested
-	@DisplayName("Pruebas de rendimiento [NFR-0002]")
-	class Rendimiento{
-		@Test 
-		void limiteTestNotificarAlertaRendimiento1() {
-			assertTimeoutPreemptively(Duration.ofSeconds(2), ()->{
-				testNotificarAlerta1();
-			});
+	class lanzarAlarma{
+		@BeforeAll
+	    static void setUpBeforeClass() throws Exception{
+	        GAS = mock(GestorAlarmas.class);
+	        cen = new Centralita(GAS);
 		}
+		@DisplayName("Prueba [CPB-001-01]")
 		@Test
-		void limiteTestNotificarAlertaRendimiento2() {
-			assertTimeoutPreemptively(Duration.ofSeconds(2), ()->{
-				testNotificarAlerta2();
-			});
+		void testNotificarAlerta1() {
+			Float[] pos = {12.3f, 12.4f};
+			Alarma alarm = new Alarma(1, "Incendio");
+			Alerta alert=new Alerta("Urgente", 1, pos, "ETSE", new Date(), alarm);
+			assertTrue(cen.notificarAlerta(alert),"Error al notificar una alerta.");
+		}
+		@DisplayName("Prueba [CPB-001-02]")
+		@Test
+		void testNotificarAlerta2() {
+			assertTrue(cen.notificarAlerta(null),"Error al notificar una alerta nula.");
+		}
+		@Nested
+		@DisplayName("Pruebas de rendimiento [NFR-0002]")
+		class Rendimiento{
+			@DisplayName("Prueba [CPB-001-03]")
+			@Test 
+			void limiteTestNotificarAlertaRendimiento1() {
+				assertTimeoutPreemptively(Duration.ofSeconds(2), ()->{
+					testNotificarAlerta1();
+				});
+			}
+			@DisplayName("Prueba [CPB-001-04]")
+			@Test
+			void limiteTestNotificarAlertaRendimiento2() {
+				assertTimeoutPreemptively(Duration.ofSeconds(2), ()->{
+					testNotificarAlerta2();
+				});
+			}
 		}
 	}
-
 }
+
